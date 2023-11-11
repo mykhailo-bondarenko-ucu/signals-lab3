46e7e8cb4baeef62f813f25396461dd1053c85a0
michael-2956 <work.michael.2956@gmail.com>
2023-11-11 17:07:23 +0200

    uncomment tasks

--- main2.py
+++ main2.py
@@ -313,11 +313,11 @@ def task_6():
 
 
 def main():
-    # task_1()
-    # task_2()
-    # task_3()
-    # task_4()
-    # task_5()
+    task_1()
+    task_2()
+    task_3()
+    task_4()
+    task_5()
     task_6()
 
 
